e083852475a57f14ceef29307efa9cce4ed36645
julia-KAERI <julia.kaeri@gmail.com>
2024-02-02 13:30:31 +0900

    20240202_1330

--- src/NAJ.jl
+++ src/NAJ.jl
@@ -1,5 +1,17 @@
 module NAJ
 
-greet() = print("Hello World!")
+include("simplepolynomials.jl")
+include("interpolations.jl")
+
+export 
+    SimplePolynomial,
+    monic,
+    derivative,
+    integrate,
+    polynomial_from_roots,
+    valdermond_polynomial,
+    lagrange_polynomial,
+    newton_polynomial,
+    Interpolator1D
 
 end # module NAJ
